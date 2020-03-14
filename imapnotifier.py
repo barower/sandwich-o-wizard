1a8239dce32b17c1fbb7d021f504bfdab10edd0c
Bartlomiej Osinski <barower@gmail.com>
2020-03-14 16:15:02 +0100

    basic login from file

--- imapnotifier.py
+++ imapnotifier.py
@@ -2,6 +2,7 @@ from imapclient import  IMAPClient
 from imapclient.exceptions import IMAPClientError
 import time
 import os
+import sys
 
 SLEEP_TIME = 1.5
 
@@ -32,10 +33,19 @@ class Folder():
         else:
             return len(self.server.search(['TO', self.recipients]))
 
+def login():
+    with open("passes") as f:
+        text = f.read().split()
+        url = text[0]
+        login = text[1]
+        password = text[2]
+        server = IMAPClient(url, use_uid=True)
+        server.login(login, password)
+        return server
 
 if __name__ == "__main__":
-    server = IMAPClient('PLACEHOLDER', use_uid=True)
-    server.login('PLACEHOLDER', 'PLACEHOLDER')
+
+    server = login()
 
     #recipientaddress = 'dzialkowa@enigma.com.pl'
     recipientaddress = None
