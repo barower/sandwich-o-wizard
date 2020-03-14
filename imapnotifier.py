440502818d52d30338fab87246f95c922978a6f7
Bartlomiej Osinski <barower@gmail.com>
2020-03-14 16:24:31 +0100

    let's take a step back

--- imapnotifier.py
+++ imapnotifier.py
@@ -8,15 +8,11 @@ SLEEP_TIME = 1.5
 
 class Folder():
 
-    def __init__(self, server, foldername, soundpath, recipients=None):
+    def __init__(self, server, foldername):
         self.server = server
         self.folder = foldername
-        self.sound = soundpath
-        self.recipients = recipients
 
         self.__create_folder()
-
-        self.msgcount = self.get_msg_count()
 
     def __create_folder(self):
         try:
@@ -24,14 +20,6 @@ class Folder():
         except IMAPClientError:
             print("Cannot find folder {}, creating..".format(self.folder))
             self.server.create_folder(self.folder)
-
-    def get_msg_count(self):
-        select_info = self.server.select_folder(self.folder)
-
-        if self.recipients is None:
-            return select_info[b'EXISTS']
-        else:
-            return len(self.server.search(['TO', self.recipients]))
 
 def login():
     with open("passes") as f:
@@ -47,20 +35,10 @@ if __name__ == "__main__":
 
     server = login()
 
-    #recipientaddress = 'dzialkowa@enigma.com.pl'
-    recipientaddress = None
-
-    Kanapki = Folder(server, "Kanapki", "")
-    Catering50 = Folder(server, "Catering50", "")
-    Slimak = Folder(server, "Slimak", "")
-    Sushi = Folder(server, "Sushi", "")
-
-    folders = [Kanapki, Catering50, Slimak, Sushi]
+    jedzenie = Folder(server, "Jedzenie")
 
     while True:
-        for folder in folders:
-            count = folder.get_msg_count()
-            print('{} messages in folder {}'.format(count, folder.folder))
+        print("Checking emails")
 
         time.sleep(SLEEP_TIME)
         os.system("clear")
