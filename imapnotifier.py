fe45cba3922ae7fb91bb47e391a0e85ea251705a
Bartlomiej Osinski <barower@gmail.com>
2020-03-14 15:58:24 +0100

    folder creation

--- imapnotifier.py
+++ imapnotifier.py
@@ -1,6 +1,9 @@
 from imapclient import  IMAPClient
+from imapclient.exceptions import IMAPClientError
 import time
 import os
+
+SLEEP_TIME = 1.5
 
 class Folder():
 
@@ -9,7 +12,17 @@ class Folder():
         self.folder = foldername
         self.sound = soundpath
         self.recipients = recipients
+
+        self.__create_folder()
+
         self.msgcount = self.get_msg_count()
+
+    def __create_folder(self):
+        try:
+            self.server.select_folder(self.folder)
+        except IMAPClientError:
+            print("Cannot find folder {}, creating..".format(self.folder))
+            self.server.create_folder(self.folder)
 
     def get_msg_count(self):
         select_info = self.server.select_folder(self.folder)
@@ -39,6 +52,6 @@ if __name__ == "__main__":
             count = folder.get_msg_count()
             print('{} messages in folder {}'.format(count, folder.folder))
 
-        time.sleep(1.5)
+        time.sleep(SLEEP_TIME)
         os.system("clear")
 
